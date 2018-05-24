0ab10b31fd8f9889024331dd328441fa8b332949
Homero Pereira Avila Junior <homero.junior@unimedflorianopolis.com.br>
2018-05-24 17:55:45 -0300

    Commit com os arquivos do projeto.

---
+++ HandPong/Game1.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace HandPong
+{    
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        Texture2D ballSprite;
+        Vector2 ballPosition = Vector2.Zero;
+        Vector2 ballSpeed = new Vector2(150, 150);
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+		  
+        protected override void LoadContent()
+        {            
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            ballSprite = Content.Load<Texture2D>("basketball");
+        }
+
+        protected override void UnloadContent()
+        {
+        
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+			
+            base.Update(gameTime);
+        }
+		        
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+            spriteBatch.Draw(ballSprite, ballPosition, Color.White);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
